219157876cacbd7930aaa4bd72a79045f84e23f1
Cytrak <149499127+Cytrak@users.noreply.github.com>
2024-04-24 09:01:08 +0300

    Reactions.js

--- commands/Reactions.js
+++ commands/Reactions.js
@@ -1,15 +1,4 @@
-/** 
 
-🇫‌🇱‌🇦‌🇸‌🇭‌-🇲‌🇩‌ 
-
-  𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
- 𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
- 𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
- 𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
- * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : 𝗙𝗹𝗮𝘀𝗵 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
- * @𝗼𝘄𝗻𝗲𝗿: 𝗙𝗿𝗮𝗻𝗰𝗲 𝗞𝗶𝗻𝗴 
- 
- **/
 
 
 
@@ -93,31 +82,3 @@ const generateReactionCommand = (reactionName, reactionEmoji) => {
 
 // ... (utilisation de la fonction generateReactionCommand pour créer des commandes de réaction)
 
-
-generateReactionCommand("bully", "👊");
-generateReactionCommand("cuddle", "🤗");
-generateReactionCommand("cry", "😢");
-generateReactionCommand("hug", "😊");
-generateReactionCommand("awoo", "🐺");
-generateReactionCommand("kiss", "😘");
-generateReactionCommand("lick", "👅");
-generateReactionCommand("pat", "👋");
-generateReactionCommand("smug", "😏");
-generateReactionCommand("bonk", "🔨");
-generateReactionCommand("yeet", "🚀");
-generateReactionCommand("blush", "😊");
-generateReactionCommand("smile", "😄");
-generateReactionCommand("wave", "👋");
-generateReactionCommand("highfive");
-generateReactionCommand("handhold");
-generateReactionCommand("nom","👅" );
-generateReactionCommand("bite", "🦷");
-generateReactionCommand("glomp", "🤗");
-generateReactionCommand("slap", "👋");
-generateReactionCommand("kill", "💀");
-generateReactionCommand("kick", "🦵");
-generateReactionCommand("happy", "😄");
-generateReactionCommand("wink", "😉");
-generateReactionCommand("poke", "👉");
-generateReactionCommand("dance", "💃");
-generateReactionCommand("cringe", "😬");
